5deb6be165b88a864a6f1221d079ab56916a4017
alleyex <alleyex@gmail.com>
2024-08-31 13:42:02 +0800

    delete all scaled files

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ if not mt5.initialize():
     print("initialize() failed, error code =", mt5.last_error())
     quit()
 
-timeframe = mt5.TIMEFRAME_D1
+timeframe = mt5.TIMEFRAME_M1
 symbol = "USDJPY"
 
 df = tool.get_historical_data(symbol, timeframe)
@@ -22,8 +22,6 @@ df = tool.create_indicators(df)
 df = tool.feature_scaling(df)
 
 tool.save_file(df, symbol, timeframe)
-
-
 
 print(df.tail())
 print(df.shape)
